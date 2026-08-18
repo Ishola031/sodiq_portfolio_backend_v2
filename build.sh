9ca301826d15e3e6f9c5f4cae4e1664c7c2132e4
Ishola031 <sannisodiq031@gmail.com>
2026-08-18 08:25:04 +0100

    update build

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
+# #!/usr/bin/env bash
+# # Exit on error
+# set -o errexit
+
+# # Collect static files
+# python manage.py collectstatic --noinput
+
+# # Run database migrations (This automatically triggers the code above!)
+# python manage.py migrate
+
+# # Start both web and worker using Honcho
+# honcho start
+
+
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
+
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Run database migrations (This automatically triggers the code above!)
+# Run database migrations
 python manage.py migrate
 
-# Start both web and worker using Honcho
-honcho start
+# Create superuser automatically if it doesn't exist
+python manage.py setup_admin
